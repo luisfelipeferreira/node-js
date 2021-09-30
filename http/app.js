de944a0f0a2c87be0afbde783d288a3b23b177cf
Luis Felipe <lipe.lftn@gmail.com>
2021-09-30 10:06:09 -0300

    adicionando express

--- http/app.js
+++ http/app.js
@@ -1,6 +1,6 @@
 var http = require("http")  
 
 http.createServer(function(req, res){
-    res.end("Hello Word! Hello to my website!")
+    res.end("Sim! Ele funciona sem internet")
 }).listen(8081);
 console.log("Ligou!")
